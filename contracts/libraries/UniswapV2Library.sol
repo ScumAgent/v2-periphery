619eedad1d5eb508c562a05a104f210766f38666
solidity12 <volibear1001@gmail.com>
2025-02-09 16:43:25 +0900

    feat: add TransferHelper

--- contracts/libraries/UniswapV2Library.sol
+++ contracts/libraries/UniswapV2Library.sol
@@ -1,6 +1,6 @@
 pragma solidity >=0.5.0;
 
-import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol';
+import '../interfaces/IUniswapV2Pair.sol';
 
 import "./SafeMath.sol";
 
@@ -21,7 +21,7 @@ library UniswapV2Library {
                 hex'ff',
                 factory,
                 keccak256(abi.encodePacked(token0, token1)),
-                hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
+                hex'1892ee6b3b8f653471529d0b06a772bdc5588bb0b15607cb427c8148f70004a9' // init code hash
             ))));
     }
 
